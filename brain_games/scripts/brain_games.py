c915d536b1a9a9f96b072d1a036a8e531173e53d
demogi4523@ <demogi4523@yandex.ru>
2021-12-21 01:38:02 +0800

    Step 3 done

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,13 @@
 #! /usr/bin/env python3
+import prompt
 
 def main():
     print("Welcome to the Brain Games!")
+    welcome_user()
+
+def welcome_user():    
+    name = prompt.string(prompt="May I have your name? ", empty=False)
+    print(f"Hello, {name}!")
 
 if __name__ == '__main__':
     main()
